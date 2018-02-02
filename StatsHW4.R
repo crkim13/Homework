bd95b5b9e3f1b45f4dbd96db72a308b6935d3fcd
crkim13 <crazycho@gmail.com>
2018-02-02 06:31:27 -0800

    3B

--- StatsHW4.R
+++ StatsHW4.R
@@ -14,3 +14,17 @@ wilcox.test(logged, unlogged, correct=TRUE)
 wilcox.test(unlogged,logged, paired=FALSE, alternative = "less", correct=TRUE)
 #Wilcoxon to get the 95% CI
 wilcox.test(unlogged,logged, paired=FALSE, correct=TRUE, conf.int=TRUE)
+
+#read EducationData.csv
+df <- read.csv(file = "~/MSDS6371/EducationData.csv", header = TRUE, sep = ",")
+head(df)
+
+#sortdf
+attach(df)
+dfsort <- df[order(-Educ, Subject ),]
+head(dfsort)
+#Welch's t-test in R
+
+t.test(Income2005 ~ Educ, data = dfsort, alternative="greater", var.equal = FALSE, paired=FALSE, conf.level = .95,)
+
+
