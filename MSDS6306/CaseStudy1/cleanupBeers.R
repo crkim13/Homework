230c38d1464f20586005c4a36b66cec96110e222
crkim13 <crazycho@gmail.com>
2018-03-21 01:40:16 -0500

    HW10

--- MSDS6306/CaseStudy1/cleanupBeers.R
+++
@@ -1,9 +0,0 @@
-Beers <- read.csv("Beers.csv", header = TRUE, sep = ",", encoding='UTF-8')
-Breweries <- read.csv("Breweries.csv", header = TRUE, sep = ",", encoding='UTF-8')
-http://www.i18nqa.com/debug/utf8-debug.html
-
-colnames(Breweries)[1] <-c("Brewery_id")
-library(plyr)
-
-
-mergeBeers<- merge(x = Breweries, y = Beers, union("Brewery_id"), all=TRUE)
